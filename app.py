c4986273ce6e872b2e689d4f53f52d3a2e8db784
sy0419 <psy041912@gmail.com>
2025-06-14 19:28:01 +0900

    Addtion of date.

--- app.py
+++ app.py
@@ -8,11 +8,13 @@ todos = []
 def home():
     if request.method == 'POST':
         title = request.form.get('title')
+        date = request.form.get('date')
         if title:
             todos.append({
                 'id': len(todos) + 1,
                 'title': title,
-                'is_completed': False
+                'is_completed': False,
+                'date': date
             })
         return redirect('/')
 
@@ -28,7 +30,7 @@ def home():
         <ul>
         {% for todo in todos %}
             <li>
-                {{ todo['title'] }} - 완료: {{ 'O' if todo['is_completed'] else 'X' }}
+                {{ todo['title'] }} ({{ todo['date'] }})  - 완료: {{ 'O' if todo['is_completed'] else 'X' }}
                 {% if not todo['is_completed'] %}
                     <form action="{{ url_for('complete_todo', todo_id=todo['id']) }}" method="post" style="display:inline;">
                         <button type="submit">완료</button>
@@ -41,6 +43,7 @@ def home():
         <h2>할 일 추가하기</h2>
         <form method="POST">
             <input type="text" name="title" placeholder="할 일을 입력하세요" required>
+            <input type="date" name="date" placeholder="날짜를 입력하세요" required>
             <button type="submit">추가</button>
         </form>
     </body>
