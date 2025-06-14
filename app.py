c0e9451336e2780042443752ec54d2a846d6d4aa
sy0419 <psy041912@gmail.com>
2025-06-14 15:29:56 +0900

    Indicate the list and add to-do list at the same time.

--- app.py
+++ app.py
@@ -1,27 +1,45 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, render_template_string, redirect
 
 app = Flask(__name__)
 
 todos = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Hello, Flask! This is the home page.'
+    if request.method == 'POST':
+        title = request.form.get('title')
+        if title:
+            todos.append({
+                'id': len(todos) + 1,
+                'title': title,
+                'is_completed': False
+            })
+        return redirect('/')  # POST 후 다시 홈으로 돌아가게 하기
 
-@app.route('/todos', methods=['GET'])
-def get_todos():
-    return jsonify(todos)
+    html = '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>할 일 목록</title>
+    </head>
+    <body>
+        <h1>할 일 목록</h1>
 
-@app.route('/todos', methods=['POST'])
-def add_todo():
-    data = request.get_json()
-    todo = {
-        'id': len(todos) + 1,
-        'title': data.get('title'),
-        'is_completed': False
-    }
-    todos.append(todo)
-    return jsonify(todo), 201
+        <ul>
+        {% for todo in todos %}
+            <li>{{ todo['title'] }} - 완료: {{ 'O' if todo['is_completed'] else 'X' }}</li>
+        {% endfor %}
+        </ul>
+
+        <h2>할 일 추가하기</h2>
+        <form method="POST">
+            <input type="text" name="title" placeholder="할 일을 입력하세요" required>
+            <button type="submit">추가</button>
+        </form>
+    </body>
+    </html>
+    '''
+    return render_template_string(html, todos=todos)
 
 if __name__ == '__main__':
     app.run(debug=True)
